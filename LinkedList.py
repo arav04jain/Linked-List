17580e823df7dde1f0f71edb05c272b6ae2ce4fe
Arav Jain <141753490+arav04jain@users.noreply.github.com>
2024-06-03 20:48:22 +0530

    Add files via upload
    initial

---
+++ LinkedList.py
@@ -0,0 +1,146 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    # self.head is the indicator to where the link list starts
+    def __init__(self):
+        self.head = None
+
+    # Prints the Linked List
+    def print_list(self):
+        if self.head is None:
+            return
+        else:
+            for x in range(self.length()):
+                print(self.element_at(x))
+
+    # Clears the list-Changes the head
+    def clear_list(self):
+        self.head = None
+
+    # Gets the Index of a particular Element
+    def get_index(self, element):
+        current_node = self.head
+        pos = 0
+        while current_node:
+            if current_node.data == element:
+                return pos
+            else:
+                pos += 1
+
+    # Gives the Node on a particular index - Not to be used by the User
+    def __node_on_index(self, index):
+        if index + 1 <= self.length():
+            current_node = self.head
+            for iterate in range(index):
+                current_node = current_node.next
+
+            return current_node
+
+        else:
+            print("Index out of Range")
+
+    # Gives the element on a particular Index
+    def element_at(self, index):
+        if index + 1 <= self.length():
+            return self.__node_on_index(index).data
+        else:
+            print("Index out of Range")
+
+    # Gives the length of the list
+    def length(self):
+        count = 0
+
+        if self.head is None:
+            return count
+        else:
+            current_node = self.head
+
+            while current_node:
+                count += 1
+                current_node = current_node.next
+
+        return count
+
+    # Inserts Elements in the beginning of the Linked List
+    def add_beg(self, data):
+
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    # Inserts Element at the end of the linked list
+    def add(self, data):
+
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            end_node = self.__node_on_index(self.length() - 1)
+            end_node.next = new_node
+
+    # Inserts element at a particular index
+    def insert(self, data, index):
+        new_node = Node(data)
+
+        if index == 0:
+            self.add_beg(data)
+        else:
+            current_node = self.__node_on_index(index - 1)
+
+            if current_node is not None:
+                new_node.next = current_node.next
+                current_node.next = new_node
+            else:
+                print('Index not present')
+
+    # Deletes the first element
+    def delete_first_element(self):
+        if self.head is None:
+            return
+        else:
+            self.head = self.head.next
+
+    def delete_last_element(self):
+        if self.head is None:
+            return
+        else:
+            self.__node_on_index(self.length() - 2).next = None
+
+    # Deletes a particular element at a certain Index
+    def delete(self, index):
+
+        if index == 0:
+            self.delete_first_element()
+        elif index == self.length() - 1:
+            self.delete_last_element()
+        else:
+            self.__node_on_index(index - 1).next = self.__node_on_index(index + 1)
+
+    # Deletes element by the Element
+    def delete_element(self, element):
+        self.delete(self.get_index(element))
+
+    # Copies the list into another
+    def copy(self):
+        copy_list = LinkedList()
+        for xx in range(self.length()):
+            copy_list.add(self.element_at(xx))
+
+        return copy_list
+
+    # Merges 2 linked lists
+    def extend(self, other_list):
+        end_element = self.__node_on_index(self.length() - 1)
+        end_element.next = other_list.head
+
+
